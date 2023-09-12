c7a8301c32eccccbd575f30ba539b5702d738d5f
Leonardo Braga <Leonardo.2233595@aluno.faculdadedescomplica.com.br>
2023-09-12 19:32:38 -0300

    Test: Include findById test

---
+++ backend/src/test/java/com/to_do_app/service/TaskServiceTest.java
@@ -0,0 +1,55 @@
+package com.to_do_app.service;
+
+import com.to_do_app.model.Task;
+import com.to_do_app.repository.TaskRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class TaskServiceTest {
+
+    @Mock
+    private TaskRepository taskRepository;
+
+    @InjectMocks
+    private TaskService taskService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        taskService = new TaskService(taskRepository);
+    }
+
+    @Test
+    @DisplayName("findById should return the task with the specified id")
+    void findById_ThenReturnTask() {
+        // Arrange
+        Task task = new Task();
+        task.setId(1L);
+        task.setTitle("Test task");
+        task.setDescription("Test description");
+        task.setCompleted(false);
+
+        when(taskRepository.findById(ArgumentMatchers.anyLong())).thenReturn(Optional.of(task));
+
+        // Act
+        Task returnedTask = taskService.findById(1L);
+
+        // Assert
+        assertSame(task, returnedTask);
+        verify(taskRepository, times(1)).findById(1L);
+    }
+
+
+
+}
