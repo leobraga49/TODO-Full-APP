ab51060cfa453ec6a310f962ce2599d4dcd29fbf
Leonardo Braga <Leonardo.2233595@aluno.faculdadedescomplica.com.br>
2023-09-13 00:32:25 -0300

    Test: Include getAllTasks

---
+++ backend/src/test/java/com/to_do_app/controller/TaskControllerTest.java
@@ -0,0 +1,53 @@
+package com.to_do_app.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.to_do_app.model.Task;
+import com.to_do_app.service.TaskService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TaskControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TaskService taskService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private List<Task> tasks;
+
+    @BeforeEach
+    void setUp() {
+        tasks = new ArrayList<>();
+        tasks.add(new Task(1L, "Test task 1", "Test description 1", false));
+        tasks.add(new Task(2L, "Test task 2", "Test description 2", false));
+    }
+
+    @Test
+    void getAllTasks() throws Exception {
+        when(taskService.findAll()).thenReturn(tasks);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/tasks"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(tasks)));
+    }
+
+}
